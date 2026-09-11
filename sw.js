b8dbb5ee82cedf51c625d92ef16f52dc86085c31
29AristotleAnthonyo <29antonioa@catholiccentral.net>
2026-09-10 21:28:13 -0400

    offline semi-appps test1

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'shambots-scouting-v1';
+const APP_FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './Assets/CCrest.png',
+  './Assets/bots.webp',
+  './node_modules/qrcodejs/qrcode.min.js',
+  './node_modules/html5-qrcode/html5-qrcode.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) return cachedResponse;
+
+      return fetch(event.request).then(networkResponse => {
+        if (!networkResponse || networkResponse.status !== 200 || networkResponse.type === 'opaque') {
+          return networkResponse;
+        }
+
+        const responseToCache = networkResponse.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseToCache));
+        return networkResponse;
+      });
+    })
+  );
+});
